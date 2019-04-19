786e9ef6b60ef01e23a2dddf726af453b1d82e27
Robin Boucher <robin.boucher@epita.fr>
2019-04-19 12:05:05 +0200

    tests: split testHangman into testAscii and testHangman

--- src/test/java/testAsciiHangman.java
+++ src/test/java/testAsciiHangman.java
@@ -1,4 +1,91 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class testAsciiHangman {
+    private Hangman hangman;
+
+    @Before
+    public void setup() {
+        hangman = new Hangman();
+        hangman.setWord("dog");
+    }
+
+    void sameLetterForLoops(String letter, int loops) {
+        for (int i = 0; i < loops; i++) {
+            hangman.giveLetter(letter);
+        }
+    }
+
+    @Test(timeout = 5000)
+    public void asciiHangmanOnePenalization() {
+        hangman.giveLetter("a");
+        Assert.assertEquals("___", hangman.getAscii());
+    }
+
+    @Test(timeout = 5000)
+    public void asciiHangmanTwoPenalizations() {
+        sameLetterForLoops("a", 2);
+        Assert.assertEquals(" | " + System.lineSeparator() + "_|_", hangman.getAscii());
+    }
+
+    @Test(timeout = 5000)
+    public void asciiHangmanThreePenalizations() {
+        sameLetterForLoops("a", 3);
+        Assert.assertEquals("  __" + System.lineSeparator() + " | " + System.lineSeparator() + "_|_",
+                hangman.getAscii());
+    }
+
+    @Test(timeout = 5000)
+    public void asciiHangmanFourPenalizations() {
+        sameLetterForLoops("a", 4);
+        Assert.assertEquals(
+                "  __" + System.lineSeparator() + " |/ " + System.lineSeparator() + "_|_",
+                hangman.getAscii());
+    }
+
+    @Test(timeout = 5000)
+    public void asciiHangmanFivePenalizations() {
+        sameLetterForLoops("a", 5);
+        Assert.assertEquals(
+                "  __" + System.lineSeparator() + " |/ |" + System.lineSeparator() + "_|_"
+                , hangman.getAscii());
+    }
+
+    @Test(timeout = 5000)
+    public void asciiHangmanSixPenalizations() {
+        sameLetterForLoops("a", 6);
+        Assert.assertEquals(
+                "  __" + System.lineSeparator() + " |/ |" + System.lineSeparator() + "_|_ O",
+                hangman.getAscii());
+    }
+
+    @Test(timeout = 5000)
+    public void asciiHangmanSevenPenalizations() {
+        sameLetterForLoops("a", 7);
+        Assert.assertEquals(
+                "  __" + System.lineSeparator() + " |/ |"
+                        + System.lineSeparator() + "_|_ O" + System.lineSeparator() + "    |",
+                hangman.getAscii());
+    }
+
+    @Test(timeout = 5000)
+    public void asciiHangmanEightPenalizations() {
+        sameLetterForLoops("a", 8);
+        Assert.assertEquals(
+                "  __" + System.lineSeparator() + " |/ |" + System.lineSeparator() + "_|_ O"
+                        + System.lineSeparator() + "   /|\\",
+                hangman.getAscii());
+    }
+
+    @Test(timeout = 5000)
+    public void asciiHangmanNinePenalizations() {
+        sameLetterForLoops("a", 9);
+        Assert.assertEquals(
+                "  __" + System.lineSeparator() + " |/ |"
+                        + System.lineSeparator() + "_|_ O"
+                        + System.lineSeparator() + "   /|\\"
+                        + System.lineSeparator() + "   / \\",
+                hangman.getAscii());
+    }
 }
--- src/test/java/testHangman.java
+++ src/test/java/testHangman.java
@@ -11,12 +11,6 @@ public class testHangman {
         hangman.setWord("dog");
     }
 
-    void sameLetterForLoops(String letter, int loops) {
-        for (int i = 0; i < loops; i++) {
-            hangman.giveLetter(letter);
-        }
-    }
-
     @Test(timeout = 5000)
     public void lineFromWordCat() {
         hangman.setWord("cat");
@@ -87,77 +81,5 @@ public class testHangman {
     @Test(timeout = 5000)
     public void wordGivenInsteadOfLetter() {
         Assert.assertEquals(LETTER_VALIDITY.NOT_A_LETTER, hangman.giveLetter("dog"));
-    }
-
-    @Test(timeout = 5000)
-    public void asciiHangmanOnePenalization() {
-        hangman.giveLetter("a");
-        Assert.assertEquals("___", hangman.getAscii());
-    }
-
-    @Test(timeout = 5000)
-    public void asciiHangmanTwoPenalizations() {
-        sameLetterForLoops("a", 2);
-        Assert.assertEquals(" | " + System.lineSeparator() + "_|_", hangman.getAscii());
-    }
-
-    @Test(timeout = 5000)
-    public void asciiHangmanThreePenalizations() {
-        sameLetterForLoops("a", 3);
-        Assert.assertEquals("  __" + System.lineSeparator() + " | " + System.lineSeparator() + "_|_",
-                hangman.getAscii());
-    }
-
-    @Test(timeout = 5000)
-    public void asciiHangmanFourPenalizations() {
-        sameLetterForLoops("a", 4);
-        Assert.assertEquals(
-                "  __" + System.lineSeparator() + " |/ " + System.lineSeparator() + "_|_",
-                hangman.getAscii());
-    }
-
-    @Test(timeout = 5000)
-    public void asciiHangmanFivePenalizations() {
-        sameLetterForLoops("a", 5);
-        Assert.assertEquals(
-                "  __" + System.lineSeparator() + " |/ |" + System.lineSeparator() + "_|_"
-                , hangman.getAscii());
-    }
-
-    @Test(timeout = 5000)
-    public void asciiHangmanSixPenalizations() {
-        sameLetterForLoops("a", 6);
-        Assert.assertEquals(
-                "  __" + System.lineSeparator() + " |/ |" + System.lineSeparator() + "_|_ O",
-                hangman.getAscii());
-    }
-
-    @Test(timeout = 5000)
-    public void asciiHangmanSevenPenalizations() {
-        sameLetterForLoops("a", 7);
-        Assert.assertEquals(
-                "  __" + System.lineSeparator() + " |/ |"
-                        + System.lineSeparator() + "_|_ O" + System.lineSeparator() + "    |",
-                hangman.getAscii());
-    }
-
-    @Test(timeout = 5000)
-    public void asciiHangmanEightPenalizations() {
-        sameLetterForLoops("a", 8);
-        Assert.assertEquals(
-                "  __" + System.lineSeparator() + " |/ |" + System.lineSeparator() + "_|_ O"
-                        + System.lineSeparator() + "   /|\\",
-                hangman.getAscii());
-    }
-
-    @Test(timeout = 5000)
-    public void asciiHangmanNinePenalizations() {
-        sameLetterForLoops("a", 9);
-        Assert.assertEquals(
-                "  __" + System.lineSeparator() + " |/ |"
-                        + System.lineSeparator() + "_|_ O"
-                        + System.lineSeparator() + "   /|\\"
-                        + System.lineSeparator() + "   / \\",
-                hangman.getAscii());
     }
 }
